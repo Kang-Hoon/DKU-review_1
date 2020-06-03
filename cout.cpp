7308ab59532cd701cd5717c00298ed5bd6a37b4f
Kang-Hoon <48863211+Kang-Hoon@users.noreply.github.com>
2020-06-03 18:32:13 +0900

    Add files via upload

---
+++ cout.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout <<"First C++ PROGRAM" << endl;
+
+    int i = 56;
+    char ch = 'S';
+    double d = 2.413;
+
+    cout << i << ch << d << endl;
+}
