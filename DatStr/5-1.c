52901bd262a015164a12a4f90287c7053d53505c
Kang-Hoon <48863211+Kang-Hoon@users.noreply.github.com>
2020-10-11 21:18:57 +0900

    Create 5-1.c

---
+++ DatStr/5-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a = recursive1(5);
+    printf("%d\n",a);
+}
+
+int recursive1(int n) {
+    printf("%d\n",n);
+    if (n<1) 
+        return 2;
+    else
+        return (2* recursive1(n-1) +1);
+    
+}
